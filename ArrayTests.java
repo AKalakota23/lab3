2679cc027a4da51903d98d0e4cc6bcefca5ffcd4
AKalakota23 <akhilkalakota@gmail.com>
2023-10-23 15:28:18 -0700

    fixed reversed methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,34 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+	// @Test 
+	// public void testReverseInPlace() {
+  //   int[] input1 = { 3 };
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{ 3 }, input1);
+	// }
 
+  // @Test 
+  // public void test2ReverseInPlace(){
+  //   int[] input2 = {2,4,3,5};
+  //   ArrayExamples.reverseInPlace(input2);
+  //   assertArrayEquals(new int[]{5,3,4,2}, input2);
+  // }
+
+  @Test 
+    public void test2Reversed() {
+    int[] input2 = {1,2,3 };
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input2));
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
 }
